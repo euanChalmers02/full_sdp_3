1841375199c810be47ddf2cb1a3fb3434d046a7f
Euan Chalmers <113519226+euanChalmers02@users.noreply.github.com>
2023-02-15 14:35:53 +0000

    Add files via upload

---
+++ Main/fnd/main.py
@@ -0,0 +1,6 @@
+from Sound.SoundSys.TextToSpeech import *
+
+if __name__ == "__main__":
+    print("Hello, World!")
+    save_msg_to_cache("Lift", 'Lifts')
+    save_msg_to_cache("Wet Floor Sign", 'WetFloor')
