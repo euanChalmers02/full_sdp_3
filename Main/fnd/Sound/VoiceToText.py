354c4ed9c7f9c1b03d020edc9b5b99c2a88a255d
Euan Chalmers <113519226+euanChalmers02@users.noreply.github.com>
2023-02-21 11:20:46 +0000

    Add files via upload
    voice first update

---
+++ Main/fnd/Sound/VoiceToText.py
@@ -0,0 +1,48 @@
+import speech_recognition as sr
+from Main.fnd.Sound.SoundSys.TextToSpeech import *
+
+
+# on button press start listening and return command
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak now")
+        r.energy_threshold = 4000
+        try:
+            audio = r.listen(source, 2)  # Starts Listening
+        except:
+            print("Sorry, couldn't hear you!")
+            return None
+    try:
+        text = r.recognize_google(audio)  # Recognizes audio in English
+        return (text)
+
+    except:  # When there is no notable speech
+        print("Sorry, couldn't hear you!")
+        return None
+
+
+COMMANDS = ["pause", "resume", "stop", "read out full", "customise beep sound", "customise voice",
+            "customise number of beeps"]
+COMMANDS_BUTTONS = {"pause": "p", "resume": "r", "stop": "s", "read out full": "m", "customise beep sound": "bs",
+                    "customise voice": "cv", "customise number of beeps": "cnb"}
+
+
+def parse(voice_command):
+    print(voice_command)
+    for o in COMMANDS:
+        if o in voice_command:
+            print("Command said is ... ", o)
+            return COMMANDS_BUTTONS[o]
+
+
+# call when the voice command is called
+def voice_wrapper():
+    res = listen()
+
+    if res is None:
+        play_msg_cache('default')
+        return None
+    else:
+        return parse(res)
+
