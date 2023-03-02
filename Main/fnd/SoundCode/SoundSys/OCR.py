5da061821a9e42a6c2aca5c0c443ef3ab8fcf05c
EuanChalmers <s2069520@ed.ac.uk>
2023-03-02 14:47:03 +0000

    update to ocr file

--- Main/fnd/SoundCode/SoundSys/OCR.py
+++ Main/fnd/SoundCode/SoundSys/OCR.py
@@ -19,6 +19,9 @@ class OCR:
         self.engine.stop()
         print('finish callback ', name, completed)
 
+    def new(self):
+        pass
+
     def textToSpeech(self):
 
         if self.text == "":
