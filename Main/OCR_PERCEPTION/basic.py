6a80ba4864c0b02b2f0acf38619dc85e26b2076b
EuanChalmers <s2069520@ed.ac.uk>
2023-03-07 15:53:21 +0000

    updates

--- Main/OCR_PERCEPTION/basic.py
+++ Main/OCR_PERCEPTION/basic.py
@@ -4,6 +4,10 @@ import os
 pytesseract.pytesseract.tesseract_cmd = '/usr/local/bin/tesseract'
 from Main.fnd.SoundCode.SoundSys.OCR import *
 
+PATH = os.path.abspath(__file__)
+ROOT = PATH.split("Main")
+PATH = ROOT[0] + "Main/SoundCode/logs/"
+
 def image_to_text(img):
     # pytesseract
     # image_path = "/Users/euanchalmers/Desktop/ocr/imges/ocrtest_shift.png"
@@ -14,3 +18,10 @@ def image_to_text(img):
     # this will create the text object and should play the identified text (allowing for pauses etc)
     OCR(text)
     # print("Complete ocr func")
+
+    # log the img (for us to analyse in future
+    if text != "":
+        from numpy import random
+        code = random.randint(100)
+        cv2.imwrite(PATH+code+'.jpg', img)
+        add_log("OCR-",code,"-",text)
--- Main/fnd/SoundCode/Buttons/ButtionChange.py
+++ Main/fnd/SoundCode/Buttons/ButtionChange.py
@@ -1,9 +1,10 @@
-import time
-
 from Main.fnd.SoundCode.SoundSys.VoiceToText import voice_wrapper_action
 from Main.fnd.SoundCode.SoundSys.DIST import distance_action_or_state
 from Main.fnd.SoundCode.Logging import *
 from Main.fnd.SoundCode.Buttons.Singleton import get_instate_of_state
+import RPi.GPIO as GPIO
+import time
+
 state = get_instate_of_state()
 
 def console_two():
@@ -16,9 +17,6 @@ def console_two():
         except EOFError as e:
             print(e)
 
-# def exit_mode():
-#     state.ocr = False
-#     state.dist = False
 
 def pause_wait_action():
     save_logs_to_file()
@@ -36,3 +34,32 @@ def check_next_func():
     else:
         print("could not find next command")
         return None
+
+# --------------------------------BUTTONS ----------------------------------------
+button1_pin = 11
+button2_pin = 13
+button3_pin = 15
+pin_out_list = {}
+
+def catch_all_callback(channel):
+    if GPIO.input(channel) == GPIO.LOW:
+        print("Button pressed")
+        cmd = pin_out_list[channel]
+        state.commandInterface(cmd)
+    return
+
+def button_setup():
+    # set up GPIO pins
+    GPIO.setmode(GPIO.BOARD)
+
+    # set up button pins as input with pull-up resistors
+    GPIO.setup(button1_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(button2_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(button3_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def button_listening():
+    # add event detection
+    GPIO.add_event_detect(button1_pin, GPIO.FALLING, callback=catch_all_callback, bouncetime=300)
+    GPIO.add_event_detect(button1_pin, GPIO.FALLING, callback=catch_all_callback, bouncetime=300)
+    GPIO.add_event_detect(button2_pin, GPIO.FALLING, callback=catch_all_callback, bouncetime=300)
+    GPIO.add_event_detect(button3_pin, GPIO.FALLING, callback=catch_all_callback, bouncetime=300)
