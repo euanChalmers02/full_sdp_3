9b21cb4e8644acf824e7d525ac5ae49f75c1db19
EuanChalmers <s2069520@ed.ac.uk>
2023-03-09 15:40:10 +0000

    playing around with python application workflow 2.0

---
+++ Main/test_local/testCmds.py
@@ -0,0 +1,35 @@
+import time
+import unittest
+
+from Main.Main.fnd.SoundCode.Buttons.Singleton import get_instate_of_state
+
+state = get_instate_of_state()
+
+
+class scan_scenarios(unittest.TestCase):
+
+    def test_assert_commands_create(self):
+        pass
+
+    def test_assert_command_change_as_expected(self):
+        state.commandInterface("AA")
+        self.assertEqual(state.get_state(), "pause")
+        state.commandInterface("AA")
+        self.assertEqual(state.get_state(), "Scan")
+        state.commandInterface("A")
+        self.assertEqual(state.get_state(), "dist")
+        state.commandInterface("A")
+        self.assertEqual(state.get_state(), "Scan+ocr")
+        return
+
+    def test_assert_handle_unknown_cmd(self):
+        # unknown / unmapped command
+        res = state.commandInterface("xyxysyws")
+        self.assertFalse(res)
+
+    def test_user_story_one(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Main/test_local/testVolume.py
@@ -0,0 +1,82 @@
+import unittest
+
+from Main.fnd.SoundCode.Customisation import *
+from Main.Main.fnd.SoundCode.SoundSys.Sound import Sound
+
+obj1 = Sound([245, 188], 2, "toilet_sign", True)
+
+
+class sound_sys_tests(unittest.TestCase):
+    def test_up_1(self):
+        # given
+        update_level_to("mac", 5)
+        # when
+        audio_driver_up()
+        # then
+        assert(int(get_audio_level("mac"))== 86)
+
+    def test_down_1(self):
+        # given
+        update_level_to("mac", 5)
+        # when
+        audio_driver_down()
+        # then
+        assert (int(get_audio_level("mac"))== 57)
+
+    def test_up_2(self):
+        # given
+        update_level_to("mac", 71)
+        # when
+        audio_driver_up()
+        audio_driver_up()
+        # then
+        self.assertTrue(int(get_audio_level("mac"))==100)
+
+    def test_down_2(self):
+        # given
+        update_level_to("mac", 5)
+        # when
+        audio_driver_down()
+        audio_driver_down()
+        # then
+        assert(int(get_audio_level("mac"))== 43)
+
+    def test_up_at_max(self):
+        # given
+        update_level_to("mac", 100)
+        # when
+        audio_driver_up()
+        # then
+        assert (int(get_audio_level("mac"))==100)
+
+    def test_down_at_min(self):
+        # given
+        update_level_to("mac", 0)
+        # when
+        audio_driver_down()
+        # then
+        assert (int(get_audio_level("mac"))==0)
+
+    def test_with_sound(self):
+        # given
+        update_level_to("mac", 100)
+        # play_msg_cache('Lifts.wav')
+        # when
+        audio_driver_down()
+        audio_driver_down()
+        audio_driver_down()
+        # then
+        # play_msg_cache('Lifts.wav')
+        assert(int(get_audio_level("mac"))== 57)
+
+    # def test_with_sound2(self):
+    #     # given
+    #     update_level_to("mac", 3)
+    #     play_msg_cache('Lifts.wav')
+    #     # when
+    #     audio_driver_up()
+    #     audio_driver_up()
+    #     audio_driver_up()
+    #     # then
+    #     play_msg_cache('Lifts.wav')
+    #     assert (int(get_audio_level("mac"))== 86)
