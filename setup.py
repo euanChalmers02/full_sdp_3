72c2c483b10cff81fa8ed6c2242d65ac2448700a
Euan Chalmers <113519226+euanChalmers02@users.noreply.github.com>
2023-03-09 15:28:10 +0000

    Create setup.py
    creating a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+# Package meta-data.
+NAME = 'full_sdp_3'
+DESCRIPTION = 'My short description for my project.'
+URL = 'xxx.example'
+EMAIL = 'me@example.com'
+AUTHOR = 'Euan Chalmers'
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'json', 'sys',
+]
+
+packages=['Main', 'tests']
